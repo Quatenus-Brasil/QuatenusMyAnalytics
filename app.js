11f593dd69f0348e09cc403de16d7039b672fd1f
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2025-07-03 14:51:03 -0300

    cron job pra chamar a função a cada 5 minutos e xml-js para converter a resposta para json

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 import "dotenv/config";
 import axios from "axios";
+import cron from "node-cron";
+import * as convert from "xml-js";
 
-// TODO: Tudo
 async function getEvents() {
   try {
     const response = await axios.get(`${process.env.BASEURL}/${process.env.TESTE}`);
 
-    console.log(response.data);
+    // Convertendo a resposta para JSON
+    // TODO: O JSON convertido está meio estranho, talvez olhar mais algumas opções do xml-js pra tentar melhorar a conversão
+    console.log(convert.xml2json(response.data, { compact: true, spaces: 4 }));
   } catch (error) {
     console.log(error);
   }
 }
 
-getEvents();
+// O código abaixo usa uma expressão cron para chamar a função getEvents() a cada 5 minutos.
+// https://tool.crontap.com/cronjob-debugger
+cron.schedule('*/5 * * * *', () => {
+  getEvents()
+})
+
+// getEvents();
